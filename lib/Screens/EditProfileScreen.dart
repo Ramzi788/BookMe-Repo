dc0a1e427f487b1db4279100e80666430378d82f
MA4LOUF <115499549+MA4LOUF@users.noreply.github.com>
2023-04-24 17:39:22 +0300

    Fixed update user info

--- lib/Screens/EditProfileScreen.dart
+++ lib/Screens/EditProfileScreen.dart
@@ -20,24 +20,24 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
   DecorationImage? setImage;
   TextEditingController _eController = TextEditingController();
   TextEditingController _pController = TextEditingController();
-  final _firstName = TextEditingController();
-  final _lastName = TextEditingController();
-  final _uController = TextEditingController();
+  late TextEditingController _firstName;
+  late TextEditingController _lastName;
+  late TextEditingController _userName;
 
   Widget bottomPopUp(){
     return Container(
         height: 100,
         width: MediaQuery.of(context).size.width,
-        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+        margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
         child: Column(
             children: [
-              Text(
+              const Text(
                 "Choose your profile picture",
                 style: TextStyle(
                   fontSize: 20.0,
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 20,
               ),
               Row(
@@ -46,11 +46,11 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                   TextButton.icon(onPressed: (){
                     takePhoto(ImageSource.camera);
                   },
-                      icon: Icon(Icons.camera_alt), label: Text("Camera", style: TextStyle(color: Colors.black))),
+                      icon: const Icon(Icons.camera_alt), label: const Text("Camera", style: TextStyle(color: Colors.black))),
                   TextButton.icon(onPressed: (){
                     takePhoto(ImageSource.gallery);
                   },
-                      icon: Icon(Icons.photo), label: Text("Gallery", style: TextStyle(color: Colors.black)))
+                      icon: const Icon(Icons.photo), label: const Text("Gallery", style: TextStyle(color: Colors.black)))
                 ],
               )
 
@@ -71,11 +71,11 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
   Widget build(BuildContext Context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(icon: Icon(Icons.arrow_back_ios), onPressed: (){Navigator.pop(context);},),
+        leading: IconButton(icon: const Icon(Icons.arrow_back_ios), onPressed: (){Navigator.pop(context);},),
         backgroundColor: theme().primaryColor,
         elevation: 0,
         toolbarHeight: 100,
-        title: Text("Edit Profile", style: TextStyle(fontSize: 25)),
+        title: const Text("Edit Profile", style: TextStyle(fontSize: 25)),
         centerTitle: true,
       ),
       backgroundColor: theme().primaryColor,
@@ -146,8 +146,8 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
               child: const Text("Personal Information", style: TextStyle(color: Colors.white, fontSize: 20),),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.only(left:25, top:10),
+          const Padding(
+            padding: EdgeInsets.only(left:25, top:10),
             child: Text("First Name", style: TextStyle(fontSize: 15, color: Colors.white),),
           ),
           Padding(
@@ -155,22 +155,23 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
               child: SizedBox(
                 height: 30,
                 child: TextFormField(
-                  controller: TextEditingController(text: registeredfName,),
+
+                  controller: _firstName = TextEditingController(text: registeredfName),
                   textInputAction: TextInputAction.next,
                   keyboardType: TextInputType.emailAddress,
                   decoration: InputDecoration(
-                      enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Color.fromARGB(255, 80, 78, 78))),
-                      focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Color.fromARGB(255, 80, 78, 78), )),
+                      enabledBorder: const UnderlineInputBorder(borderSide: BorderSide(color: Color.fromARGB(255, 80, 78, 78))),
+                      focusedBorder: const UnderlineInputBorder(borderSide: BorderSide(color: Color.fromARGB(255, 80, 78, 78), )),
                       fillColor: theme().primaryColor,
                       filled: true,
-                      contentPadding: EdgeInsets.only(bottom: 5, left: 2)
+                      contentPadding: const EdgeInsets.only(bottom: 5, left: 2)
                   ),
                   style: TextStyle(color:Colors.grey.shade300, fontSize: 13),
                 ),
 
               )),
-          Padding(
-            padding: const EdgeInsets.only(left:25, top:20),
+          const Padding(
+            padding: EdgeInsets.only(left:25, top:20),
             child: Text("Last Name", style: TextStyle(fontSize: 15, color: Colors.white),),
           ),
           Padding(
@@ -178,22 +179,22 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
               child: SizedBox(
                 height: 30,
                 child: TextFormField(
-                  controller: TextEditingController(text: registeredlName,),
+                  controller: _lastName = TextEditingController(text: registeredlName),
                   textInputAction: TextInputAction.next,
                   keyboardType: TextInputType.emailAddress,
                   decoration: InputDecoration(
-                      enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Color.fromARGB(255, 80, 78, 78))),
-                      focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Color.fromARGB(255, 80, 78, 78))),
+                      enabledBorder: const UnderlineInputBorder(borderSide: BorderSide(color: Color.fromARGB(255, 80, 78, 78))),
+                      focusedBorder: const UnderlineInputBorder(borderSide: BorderSide(color: Color.fromARGB(255, 80, 78, 78))),
                       fillColor: theme().primaryColor,
                       filled: true,
-                      contentPadding: EdgeInsets.only(bottom: 5, left: 2)
+                      contentPadding: const EdgeInsets.only(bottom: 5, left: 2)
                   ),
                   style: TextStyle(color:Colors.grey.shade300, fontSize: 13),
                 ),
 
               )),
-          Padding(
-            padding: const EdgeInsets.only(left:25, top:20,),
+          const Padding(
+            padding: EdgeInsets.only(left:25, top:20,),
             child: Text("Major", style: TextStyle(fontSize: 15, color: Colors.white),),
           ),
           Padding(
@@ -205,10 +206,10 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                   textInputAction: TextInputAction.next,
                   decoration: InputDecoration(
                       enabledBorder: const UnderlineInputBorder(borderSide: BorderSide(color: Color.fromARGB(255, 80, 78, 78))),
-                      focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Color.fromARGB(255, 80, 78, 78))),
+                      focusedBorder: const UnderlineInputBorder(borderSide: BorderSide(color: Color.fromARGB(255, 80, 78, 78))),
                       fillColor: theme().primaryColor,
                       filled: true,
-                      contentPadding: EdgeInsets.only(bottom: 5, left: 2)
+                      contentPadding: const EdgeInsets.only(bottom: 5, left: 2)
 
                   ),
                   style: TextStyle(color:Colors.grey.shade300, fontSize: 13),
@@ -222,11 +223,11 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
               padding: const EdgeInsets.only(left: 5, bottom: 10),
               color: theme().primaryColor,
               width: 100,
-              child: Text("Account Information", style: TextStyle(color: Colors.white, fontSize: 20),),
+              child: const Text("Account Information", style: TextStyle(color: Colors.white, fontSize: 20),),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.only(left:25, top:10),
+          const Padding(
+            padding: EdgeInsets.only(left:25, top:10),
             child: Text("Email Address", style: TextStyle(fontSize: 15, color: Colors.white),),
           ),
           Padding(
@@ -238,19 +239,19 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                   textInputAction: TextInputAction.next,
                   keyboardType: TextInputType.emailAddress,
                   decoration: InputDecoration(
-                      enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Color.fromARGB(255, 80, 78, 78))),
-                      focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Color.fromARGB(255, 80, 78, 78))),
+                      enabledBorder: const UnderlineInputBorder(borderSide: BorderSide(color: Color.fromARGB(255, 80, 78, 78))),
+                      focusedBorder: const UnderlineInputBorder(borderSide: BorderSide(color: Color.fromARGB(255, 80, 78, 78))),
                       fillColor: theme().primaryColor,
                       filled: true,
-                      contentPadding: EdgeInsets.only(bottom: 5, left: 2)
+                      contentPadding: const EdgeInsets.only(bottom: 5, left: 2)
 
                   ),
                   style: TextStyle(color:Colors.grey.shade300, fontSize: 13),
                 ),
 
               )),
-          Padding(
-            padding: const EdgeInsets.only(left:25, top:20),
+          const Padding(
+            padding: EdgeInsets.only(left:25, top:20),
             child: Text("Username", style: TextStyle(fontSize: 15, color: Colors.white),),
           ),
           Padding(
@@ -259,16 +260,16 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                 height: 30,
                 child: TextFormField(
 
-                  controller: TextEditingController(text: registeredUsername,),
+                  controller: _userName = TextEditingController(text: registeredUsername),
                   textInputAction: TextInputAction.next,
                   keyboardType: TextInputType.emailAddress,
 
                   decoration: InputDecoration(
                       enabledBorder: const UnderlineInputBorder(borderSide: BorderSide(color: Color.fromARGB(255, 80, 78, 78))),
-                      focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Color.fromARGB(255, 80, 78, 78))),
+                      focusedBorder: const UnderlineInputBorder(borderSide: BorderSide(color: Color.fromARGB(255, 80, 78, 78))),
                       fillColor: theme().primaryColor,
                       filled: true,
-                      contentPadding: EdgeInsets.only(bottom: 5, left: 2)
+                      contentPadding: const EdgeInsets.only(bottom: 5, left: 2)
                   ),
                   style: TextStyle(color:Colors.grey.shade300, fontSize: 13),
                 ),
@@ -291,7 +292,12 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                     setState(() {
                       finalPass = _pController.text;
                     });
-                    Navigator.pop(context);
+                    userData.doc(registeredEmail).update({
+                      'username': _userName.text,
+                      'fname': _firstName.text,
+                      'lname': _lastName.text
+                    }).then((value) => fetchUserData());
+                    Navigator.pushNamed(context, '/homepage');
                   },
                   child: const Text("Confirm Changes", style: TextStyle(color: Colors.white),)
               ),
